7127e4bbd040d4f26c81f070a62acc82bbb9ed30
Emil Jamel Mahmuda <memiljamel@gmail.com>
2024-09-28 17:41:44 +0700

    feat: add authentication feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ResetController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DistributorController;
@@ -18,9 +22,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('categories', CategoryController::class);
-Route::resource('brands', BrandController::class);
-Route::resource('distributors', DistributorController::class);
-Route::resource('responsible-persons', ResponsiblePersonController::class)
-    ->parameters(['responsible-persons' => 'person']);
-Route::resource('locations', LocationController::class);
+Route::middleware('guest')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::controller(LoginController::class)->group(function () {
+            Route::get('login', 'index')
+                ->name('login.index');
+            Route::post('login/authenticate', 'authenticate')
+                ->name('login.authenticate');
+        });
+
+        Route::controller(ForgotController::class)->group(function () {
+            Route::get('forgot-password', 'index')
+                ->name('forgot.index');
+            Route::post('forgot-password/send', 'send')
+                ->name('forgot.send');
+        });
+
+        Route::controller(ResetController::class)->group(function () {
+            Route::get('reset-password', 'index')
+                ->name('reset.index');
+            Route::post('reset-password/recovery', 'recovery')
+                ->name('reset.recovery');
+        });
+    });
+});
+
+Route::middleware(['auth', 'verified', 'auth.session'])->group(function () {
+    Route::resources([
+        'categories' => CategoryController::class,
+        'brands' => BrandController::class,
+        'distributors' => DistributorController::class,
+        'locations' => LocationController::class,
+    ]);
+
+    Route::resource('responsible-persons', ResponsiblePersonController::class)
+        ->parameters(['responsible-persons' => 'person']);
+
+    Route::prefix('auth')->group(function () {
+        Route::delete('logout', LogoutController::class)
+            ->name('logout');
+    });
+});
